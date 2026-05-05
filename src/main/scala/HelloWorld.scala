039c3a56dc69a7dc2cbc063425a692c66df7724d
Alejandro <armentaayalaalejandro@gmail.com>
2026-05-04 22:46:26 -0600

    commit

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,21 +1,21 @@
 import org.apache.spark.sql.SparkSession
 
-import org.apache.spark.sql.types.{StructField,StructType,StringType,LongType}
-import org.apache.spark.sql.types.Metadata
+import org.apache.spark.sql.types.{StructField,StructType,StringType,LongType,Metadata}
 
-import org.apache.spark.sql.functions.{window,col,desc,column,max,date_format}
+import org.apache.spark.sql.Row
+
+import org.apache.spark.sql.functions.{
+    window,col,desc,column,max,date_format,expr,lit
+    }
+
+
 import org.apache.spark.ml.feature.{StringIndexer,OneHotEncoder,VectorAssembler}
+
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
 
 object HelloWorld {
-
-    case class Flight(
-        dest_country_name: String,
-        origin_country_name: String,
-        count: BigInt
-    )
 
     def main(args: Array[String]) : Unit =
     {
@@ -53,6 +53,34 @@ object HelloWorld {
         load("./data/flight-data/json/2015-summary.json")
 
         df.printSchema()
+
+
+        df.selectExpr(
+            "*", 
+            "(dest_country_name = origin_country_name) as withinCountry"
+        ).
+        show(false)
+
+
+        //df.selectExpr("dest_country_name as destination", "dest_country_name").show
+
+
+        df.selectExpr("avg(count)","count(distinct(dest_country_name))").show(false)
+
+        df.select(expr("*"),lit(1).as("ale")).show(false)
+
+        df.withColumn("ale", lit(1)).show(false)
+
+
+        df.withColumn("withinCountry", expr("dest_country_name == origin_country_name")).show(false)
+
+        val a = df.columns
+        a.foreach(println)
+
+        df.withColumnRenamed("DEST_COUNTRY_NAME","ale").show
+
+
+
 
 
         
